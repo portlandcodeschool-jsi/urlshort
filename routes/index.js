7c84d2a7b6b0c9aa074ab5b4c197c8acfabb277e
Erin Call <hello@erincall.com>
2015-06-10 12:29:28 -0700

    A little bit of documentation.

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,19 @@ router.get('/', function(request, response) {
   response.render('index', {});
 });
 
+// request handler to create a new shortened url
 router.post('/', function(request, response) {
   var database = app.get('database');
 
   var urls = database.collection('urls');
   var key = uuid.v4();
 
+  /*
+  insert the new url.
+  "key" is the uuid, the shortened version of the url.
+  "target" is the url to which the user should be redirected.
+  "count" is the number of times a user has been redirected.
+  */
   urls.insert({target: request.body.url, key: key, count: 0}, function(error) {
     if (error) {
       throw error;
@@ -26,6 +33,12 @@ router.post('/', function(request, response) {
   });
 });
 
+/*
+Show info about a url.
+
+The "s" variable is for pluralizing in the template: if the url has redirected once, we want to say
+    "1 time"; otherwise we want to say "n times".
+*/
 router.get('/:key/info', function(request, response) {
   var database = app.get('database');
   var urls = database.collection('urls');
@@ -49,6 +62,13 @@ router.get('/:key/info', function(request, response) {
   });
 });
 
+/*
+Redirect to the target URL.
+Increments the "count" attribute with an "atomic increment".
+Notice that the response.redirect isn't in a callback to the call to update. The
+behavior of the redirect doesn't have anything to do with the update, so we can send the
+increment command off into the world and immediately redirect the user.
+*/
 router.get('/:key', function(request, response) {
   var database = app.get('database');
   var urls = database.collection('urls');
